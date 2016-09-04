edcd1e5d663dab52f638c4ccb05da8f5fe44fba8
dragon <rongjihuang@gmail.com>
2016-09-04 16:02:52 +0800

    maven config for 2 modules

---
+++ m1/src/main/java/com/rj/gradle/M1Utils.java
@@ -0,0 +1,15 @@
+package com.rj.gradle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author dragon 2016-09-04
+ */
+public class M1Utils {
+	private final static Logger logger = LoggerFactory.getLogger(M1Utils.class);
+
+	public void log() {
+		logger.warn("M1Utils.log");
+	}
+}
---
+++ m2/src/main/java/com/rj/gradle/M2Utils.java
@@ -0,0 +1,15 @@
+package com.rj.gradle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author dragon 2016-09-04
+ */
+public class M2Utils {
+	private final static Logger logger = LoggerFactory.getLogger(M2Utils.class);
+
+	public void log() {
+		logger.warn("M2Utils.log");
+	}
+}
